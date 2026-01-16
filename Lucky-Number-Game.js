0cc8910b56294e6d6e00819a6077893838f72019
Mastermou8 <mouperalta@gmail.com>
2026-01-16 15:38:00 -0500

    Added lucky Number game Files

---
+++ Lucky-Number-Game.js
@@ -0,0 +1,19 @@
+<HTML, Lang="en">
+	<HEAD>
+		<META CHARSET-"UTF-8">
+		<TITLE>LuckyNumberGame</TITLE>
+		<SCRIPT>
+			CLASS main{
+				
+			}
+		</SCRIPT>
+	</HEAD>
+	<BODY onclick-"onclick();">
+		<HEADER><H1>Lucky Number Game</H1></HEADER>
+		<SECTION id - "console"></SECTION>
+		<SCRIPT>
+			myMain = new main();
+		<SCRIPT>
+	
+	</BODY>
+</HTML>
